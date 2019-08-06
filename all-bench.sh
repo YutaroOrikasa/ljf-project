2d58c37392ddc9d59f6bc54a5f84d7666b036001
Yutaro Orikasa <yutaro.orikasa.625@gmail.com>
2019-08-06 22:12:15 +0900

    Print command line of benchmark in "make all-bench"

--- all-bench.sh
+++ all-bench.sh
@@ -2,9 +2,11 @@
 
 for ll_file in $LL_FILES;do
     echo "####### $ll_file #######"
+    printf 'command line: "%s" "%s" "%s"\n' "$BUILD_DIR"/main "$ll_file" "$FLAGS"
     "$BUILD_DIR"/main "$ll_file" "$FLAGS" || {
         status=$?
         echo "****** benchmark CRASHED: $ll_file *******"
+        printf 'command line: "%s" "%s" "%s"\n' "$BUILD_DIR"/main "$ll_file" "$FLAGS"
         exit $status
     }
 done
