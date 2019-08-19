fc77be676ba269955c40f68aec64feb87384a2c8
Yutaro Orikasa <yutaro.orikasa.625@gmail.com>
2019-08-19 22:41:01 +0900

    Add runtime/config.hpp

---
+++ runtime/config.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+// User's custom configuration is implicitly incluted by CONFIG_FILE Makefile variable
+// and -include clang option.
+
+#if !defined(LJF_CALCULATE_TYPE)
+#define LJF_CALCULATE_TYPE false
+#endif // LJF_CALCULATE_TYPE
+
+namespace ljf::config
+{
+static constexpr bool calculate_type = LJF_CALCULATE_TYPE;
+#undef LJF_CALCULATE_TYPE
+} // namespace ljf::config
