b821b815d2b93283bd1d27608d59fff4afee1dc1
Yutaro Orikasa <yutaro.orikasa.625@gmail.com>
2019-08-27 03:32:02 +0900

    Implement BrokenIterator unittest

--- runtime/runtime.cpp
+++ runtime/runtime.cpp
@@ -653,8 +653,14 @@ TEST(ObjectIterator, BrokenIter)
     ljf_set_object_to_table(obj.get(), "elem", elem.get());
 
     auto iter = obj->iter_hash_table();
-    EXPECT_EQ(iter.get().key, "elem");
-    EXPECT_EQ(iter.get().value, elem);
+    EXPECT_NO_THROW(iter.get());
+    EXPECT_NO_THROW(iter.next());
+
+    auto iter2 = obj->iter_hash_table();
+    ljf_set_object_to_table(obj.get(), "elem2", elem.get());
+
+    EXPECT_THROW(iter2.get(), BrokenIteratorError);
+    EXPECT_THROW(iter2.next(), BrokenIteratorError);
 }
 
 using TemporaryStorage = Object;
