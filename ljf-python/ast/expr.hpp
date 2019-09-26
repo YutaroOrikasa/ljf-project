0c05c1a50e18ac46a9b0593a3005df7b51d9c321
Yutaro Orikasa <yutaro.orikasa.625@gmail.com>
2019-09-26 16:04:12 +0900

    [ljf-python][ast] Move Expr class to Expr.hpp

--- ljf-python/ast/expr.hpp
+++ ljf-python/ast/expr.hpp
@@ -10,48 +10,14 @@
 
 #include <cassert>
 
-#include "./expr/Expr.hpp"
+#include "../Token.hpp"
+
+#include "expr/Expr.hpp"
+
+#include "expr/literal.hpp"
 
 namespace ljf::python::ast
 {
-struct ExprVariant;
-
-class Expr
-{
-private:
-    std::unique_ptr<ExprVariant> expr_var_ptr_;
-
-public:
-    template <typename T,
-              typename = typename std::remove_reference_t<T>::is_expr_impl>
-    /*implicit*/ Expr(T &&t)
-    {
-        expr_var_ptr_ = std::make_unique<ExprVariant>(std::forward<T>(t));
-    }
-
-    Expr(const Expr &other)
-    {
-        expr_var_ptr_ = std::make_unique<ExprVariant>(*other.expr_var_ptr_);
-    }
-
-    Expr(Expr &&) = default;
-
-    Expr &operator=(const Expr &other)
-    {
-        return *this = Expr(other);
-    }
-
-    Expr &operator=(Expr &&) = default;
-
-    template <typename Visitor>
-    auto accept(Visitor &&visitor) const
-    {
-        assert(expr_var_ptr_);
-        return std::visit(std::forward<Visitor>(visitor), *expr_var_ptr_);
-    }
-};
-
-static_assert(std::is_copy_constructible_v<Expr>);
 
 class StringLiteralExpr
 {
