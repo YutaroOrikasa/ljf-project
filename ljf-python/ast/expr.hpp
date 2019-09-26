b3dfbf5a9a20594482f1b85d4a216d81adef9891
Yutaro Orikasa <yutaro.orikasa.625@gmail.com>
2019-09-26 16:11:01 +0900

    [ljf-python][ast] Move literal expr classes to literal.hpp

--- ljf-python/ast/expr.hpp
+++ ljf-python/ast/expr.hpp
@@ -19,36 +19,6 @@
 namespace ljf::python::ast
 {
 
-class StringLiteralExpr
-{
-private:
-    Token token_;
-
-public:
-    using is_expr_impl = void;
-    StringLiteralExpr(Token &&token) : token_(std::move(token)) {}
-
-    const Token &token() const noexcept
-    {
-        return token_;
-    }
-};
-
-class IntegerLiteralExpr
-{
-private:
-    Token token_;
-
-public:
-    using is_expr_impl = void;
-    IntegerLiteralExpr(Token &&token) : token_(std::move(token)) {}
-
-    const Token &token() const noexcept
-    {
-        return token_;
-    }
-};
-
 namespace detail
 {
 class SingleTokenExpr
--- ljf-python/ast/expr/literal.hpp
+++ ljf-python/ast/expr/literal.hpp
@@ -1,6 +1,50 @@
 #pragma once
 
+#include <tuple>
+#include <variant>
+#include <optional>
+#include <string>
+#include <vector>
+#include <memory>
+#include <type_traits>
+
+#include <cassert>
+
+#include "../../Token.hpp"
+
+#include "Expr.hpp"
+
 namespace ljf::python::ast
 {
+
+class StringLiteralExpr
+{
+private:
+    Token token_;
+
+public:
+    using is_expr_impl = void;
+    StringLiteralExpr(Token &&token) : token_(std::move(token)) {}
+
+    const Token &token() const noexcept
+    {
+        return token_;
+    }
+};
+
+class IntegerLiteralExpr
+{
+private:
+    Token token_;
+
+public:
+    using is_expr_impl = void;
+    IntegerLiteralExpr(Token &&token) : token_(std::move(token)) {}
+
+    const Token &token() const noexcept
+    {
+        return token_;
+    }
+};
 
 } // namespace ljf::python::ast
