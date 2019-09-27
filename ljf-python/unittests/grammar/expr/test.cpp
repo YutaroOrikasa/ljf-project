917b93b6d3cd6324840ea0c6dfdda100fee6a2ae
Yutaro Orikasa <yutaro.orikasa.625@gmail.com>
2019-09-27 19:04:53 +0900

    [ljf-python][grammar] Add unittests

--- ljf-python/unittests/grammar/expr/test.cpp
+++ ljf-python/unittests/grammar/expr/test.cpp
@@ -7,10 +7,57 @@
 using namespace ljf::python;
 using namespace ljf::python::grammar;
 
+using namespace ljf::python::parser;
+
+static ExprGrammars<SStreamTokenStream> eg;
+
+template <typename Parser, typename TokenStream>
+static auto parse_until_eof(const Parser &p, TokenStream &stream)
+{
+    return (p + separator(eof))(stream);
+}
+
+template <typename Parser>
+static auto parse_until_end(const Parser &p, std::string input)
+{
+    SStreamTokenStream ts{input};
+    return parse_until_eof(p, ts);
+}
+
 TEST(TestExpr, Ident)
 {
-    SStreamTokenStream ssts{"a"};
+    SStreamTokenStream ts{"a"};
     ExprGrammars<SStreamTokenStream> eg;
-    auto result = eg.test(ssts);
+    auto result = eg.test(ts);
     ASSERT_TRUE(result);
 }
+
+TEST(TestListExpr, TwoItems)
+{
+    SStreamTokenStream ts{"a, b"};
+    ExprGrammars<SStreamTokenStream> eg;
+    auto result = eg.testlist(ts);
+    ASSERT_TRUE(result);
+}
+
+TEST(TestListExpr, BadInputTwoIdent)
+{
+    auto result = parse_until_end(eg.testlist, "a b");
+    ASSERT_FALSE(result);
+}
+
+TEST(TestNoCondExpr, BadInputCondExpr)
+{
+    SStreamTokenStream ts{"x if a else y"};
+    ExprGrammars<SStreamTokenStream> eg;
+    auto result = parse_until_eof(eg.test_nocond, ts);
+    ASSERT_FALSE(result);
+}
+
+// TEST(CondExpr, CondExpr)
+// {
+//     SStreamTokenStream ts{"x if a else y"};
+//     ExprGrammars<SStreamTokenStream> eg;
+//     auto result = eg.testlist(ts);
+//     ASSERT_TRUE(result);
+// }
