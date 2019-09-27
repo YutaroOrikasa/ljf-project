df41bf043517bf7282b686e6c6de832ad47e99b2
Yutaro Orikasa <yutaro.orikasa.625@gmail.com>
2019-09-27 16:31:17 +0900

    [ljf-python] Fix makefile
    - Header dependencies are correctly detected.
    - Executables (eg. rppl) are put at build/ljf-python/bin/*

--- ljf-python.mk
+++ ljf-python.mk
@@ -17,9 +17,9 @@ SOURCE_FILES = $(EXE_SOURCE_FILES) $(GRAMMAR_SOURCE_FILES) $(UNITTEST_SOURCE_FIL
 UNITTEST_OBJECT_FILES = $(UNITTEST_SOURCE_FILES:%=$(BUILD_DIR)/%.o)
 UNITTEST_EXECUTABLE = $(BUILD_DIR)/ljf-python/unittest
 
-EXECUTABLE_FILES = $(EXE_SOURCE_FILES:%.cpp=$(BUILD_DIR)/%)
+EXECUTABLE_FILES = $(EXE_SOURCE_FILES:ljf-python/%.cpp=$(BUILD_DIR)/ljf-python/bin/%)
 
-DEPENDENCY_FILES := $(SOURCE_FILES:%.cpp=$(BUILD_DIR)/%.d)
+DEPENDENCY_FILES := $(SOURCE_FILES:%=$(BUILD_DIR)/%.d)
 
 
 _DEP_FLAGS := -MMD -MP
@@ -27,12 +27,12 @@ override CXXFLAGS += -Wall -std=c++17 $(_DEP_FLAGS) $(INCLUDE_FLAGS)
 
 all: $(EXECUTABLE_FILES)
 
-$(BUILD_DIR)/ljf-python/%: ljf-python/%.cpp
-	mkdir -p $(BUILD_DIR)/ljf-python
+$(BUILD_DIR)/ljf-python/bin/%: $(BUILD_DIR)/ljf-python/%.o
+	mkdir -p $(@D)
 	$(CXX) $(CXXFLAGS) -fno-exceptions $< -o $@
 
-$(BUILD_DIR)/ljf-python/grammar/%.o: ljf-python/grammar/%.cpp
-	mkdir -p $(BUILD_DIR)/ljf-python/grammar
+$(BUILD_DIR)/ljf-python/%.o: ljf-python/%.cpp
+	mkdir -p $(BUILD_DIR)/ljf-python
 	$(CXX) $(CXXFLAGS) -c -fno-exceptions $< -o $@
 
 # $(BUILD_DIR)/ljf-python/rppl: \
@@ -44,13 +44,13 @@ $(BUILD_DIR)/ljf-python/grammar/%.o: ljf-python/grammar/%.cpp
 # 										$(BUILD_DIR)/ljf-python/grammar/expr.o \
 # 										ljf-python/rppl.cpp -o $@
 
-$(BUILD_DIR)/ljf-python/rppl: ljf-python/rppl.cpp
+$(BUILD_DIR)/ljf-python/bin/rppl: ljf-python/rppl.cpp
 	mkdir -p $(BUILD_DIR)/ljf-python
 	$(CXX) $(CXXFLAGS) -fno-exceptions ljf-python/rppl.cpp -o $@
 
 $(BUILD_DIR)/ljf-python/unittests/%.cpp.o: ljf-python/unittests/%.cpp
 	mkdir -p $(@D)
-	$(CXX) $(CXXFLAGS) -c -fno-exceptions $^ -o $@
+	$(CXX) $(CXXFLAGS) -c -fno-exceptions $< -o $@
 
 $(UNITTEST_EXECUTABLE): $(UNITTEST_OBJECT_FILES)
 	mkdir -p $(@D)
