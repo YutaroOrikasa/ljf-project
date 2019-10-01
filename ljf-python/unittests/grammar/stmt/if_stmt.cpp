1d4af7ddbf4eeebc15350362f2334216393dd248
Yutaro Orikasa <yutaro.orikasa.625@gmail.com>
2019-10-01 22:43:57 +0900

    [ljf-python][grammar] Add IfElse unittest

--- ljf-python/unittests/grammar/stmt/if_stmt.cpp
+++ ljf-python/unittests/grammar/stmt/if_stmt.cpp
@@ -7,3 +7,15 @@ TEST(IfStmt, OneLine)
     auto result = parse_until_end(sg.if_stmt, "if True: a = 0");
     ASSERT_TRUE(result) << result.error();
 }
+
+TEST(IfStmt, IfElse)
+{
+    constexpr auto input = R"(
+if x:
+    a = 0
+else:
+    a = 1
+)";
+    auto result = parse_until_end(sg.if_stmt, input);
+    ASSERT_TRUE(result) << result.error();
+}
