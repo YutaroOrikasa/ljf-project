8e5f01b329f29f689bc1d7f1d932c4320ee2ac17
Yutaro Orikasa <yutaro.orikasa.625@gmail.com>
2019-10-23 19:48:05 +0900

    [ljf-python][grammar] Add break/continue unittests for for_stmt parser

--- ljf-python/unittests/grammar/stmt/for_stmt.cpp
+++ ljf-python/unittests/grammar/stmt/for_stmt.cpp
@@ -12,3 +12,35 @@ for i in range(10):
     auto result = parse_until_end(sg.for_stmt, input);
     ASSERT_TRUE(result) << result.error();
 }
+
+TEST(ForStmt, Break)
+{
+    constexpr auto input = R"(
+for i in range(10):
+    break
+)";
+    auto result = parse_until_end(sg.for_stmt, input);
+    ASSERT_TRUE(result) << result.error();
+}
+
+TEST(ForStmt, Continue)
+{
+    constexpr auto input = R"(
+for i in range(10):
+    continue
+)";
+    auto result = parse_until_end(sg.for_stmt, input);
+    ASSERT_TRUE(result) << result.error();
+}
+
+TEST(ForStmt, ContinueInIfStmt)
+{
+    constexpr auto input = R"(
+for i in range(10):
+    if i % 2 == 1:
+        continue
+    print(i)
+)";
+    auto result = parse_until_end(sg.for_stmt, input);
+    ASSERT_TRUE(result) << result.error();
+}
