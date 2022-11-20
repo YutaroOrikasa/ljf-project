5df36d43b28d293f4624cde7da54a33ba06f55c9
Yutaro Orikasa <yutaro.orikasa.625@gmail.com>
2022-11-21 08:30:29 +0900

    [ljf-python] Add file_input test for test many grammar

--- ljf-python/unittests/grammar/stmt/file_input.cpp
+++ ljf-python/unittests/grammar/stmt/file_input.cpp
@@ -31,3 +31,25 @@ pass
     ASSERT_EQ(3, result.success().stmt_list_.size());
 
 }
+
+TEST(FileInput, All)
+{
+    constexpr auto input = R"(
+import a
+from .a import b
+
+1 + 1
+
+class A:
+    def f(self):
+        for i in range(10):
+            if x == 1:
+                continue
+            elif x == 2:
+                break
+            else:
+                pass
+)";
+    auto result = parse_until_end(sg.file_input, input);
+    ASSERT_TRUE(result) << result.error();
+}
