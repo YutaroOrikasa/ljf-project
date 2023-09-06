c5e9b10e42ebd76c19b635ceeb816c2e82f15015
Yutaro Orikasa <yutaro.orikasa.625@gmail.com>
2023-09-06 00:58:37 +0000

    [runtime] Add comment

--- include/ljf/runtime.hpp
+++ include/ljf/runtime.hpp
@@ -30,6 +30,7 @@ ljf::Object *ljf_get(ljf::Object *obj, const char *key,
 void ljf_set(ljf::Object *obj, const char *key, ljf::Object *value,
              ljf::TableVisiblity visiblity);
 
+/**************** function API ***************/
 ljf::FunctionId ljf_get_function_id_from_function_table(ljf::Object *obj,
                                                         const char *key);
 void ljf_set_function_id_to_function_table(ljf::Object *obj, const char *key,
@@ -38,16 +39,21 @@ void ljf_set_function_id_to_function_table(ljf::Object *obj, const char *key,
 ljf::Object *ljf_call_function(ljf::FunctionId function_id, ljf::Object *env,
                                ljf::Object *arg);
 
+/**************** new API ***************/
 ljf::Object *ljf_new_object();
 
 ljf::Object *ljf_new_object_with_native_data(uint64_t data);
+
+
 uint64_t ljf_get_native_data(const ljf::Object *obj);
 
+/**************** environment API ***************/
 ljf::Object *ljf_environment_get(ljf::Environment *env, const char *key,
                                  ljf::TableVisiblity vis);
 void ljf_environment_set(ljf::Environment *env, const char *key,
                          ljf::Object *value, ljf::TableVisiblity vis);
 
+/**************** function registration API ***************/
 ljf::FunctionId ljf_register_native_function(
     ljf::Object *(*fn)(ljf::Object *env, ljf::Object *tmp));
 
