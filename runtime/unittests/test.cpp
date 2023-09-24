92f4ffda5a249e1e1e4f82ae57ea6850c366d705
Yutaro Orikasa <yutaro.orikasa.625@gmail.com>
2023-09-24 16:48:50 +0900

    Refactor

--- runtime/unittests/test.cpp
+++ runtime/unittests/test.cpp
@@ -8,18 +8,33 @@
 using namespace ljf;
 using namespace ljf::internal;
 
-TEST(LJFSetGet, SetGetVisible) {
+namespace
+{
+struct LJFSetGet : public ::testing::Test {
     ObjectWrapper obj = make_new_wrapped_object();
     ObjectWrapper elem = make_new_wrapped_object();
-    obj.set("elem", elem);
+};
+} // namespace
+
+namespace
+{
+struct LJFEnvironment : public ::testing::Test {
+    std::unique_ptr<ljf::Context> ctx = make_temporary_context();
+    ObjectHolder obj = make_new_held_object();
+    ObjectHolder env0 = create_environment(ctx.get());
+    ObjectHolder env = create_callee_environment(env0, nullptr);
+};
+} // namespace
+
+
+TEST_F(LJFSetGet, SetGetVisible) {
+     obj.set("elem", elem);
 
     ASSERT_EQ(elem, obj.get("elem"));
     ASSERT_THROW(obj.get_hidden("elem"), std::out_of_range);
 }
 
-TEST(LJFSetGet, SetGetHidden) {
-    ObjectWrapper obj = make_new_wrapped_object();
-    ObjectWrapper elem = make_new_wrapped_object();
+TEST_F(LJFSetGet, SetGetHidden) {
     obj.set_hidden("elem", elem);
 
     ASSERT_EQ(elem, obj.get_hidden("elem"));
@@ -27,24 +42,18 @@ TEST(LJFSetGet, SetGetHidden) {
     ASSERT_THROW(obj.get("elem"), std::out_of_range);
 }
 
-TEST(LJFEnvironment, GetOuterValue) {
-    using namespace ljf::internal;
-    auto ctx = make_temporary_context();
-    ObjectHolder obj = make_new_held_object();
-    ObjectHolder env0 = create_environment(ctx.get());
+TEST_F(LJFEnvironment, GetOuterValue) {
+
+
     ljf_environment_set(ctx.get(), env0, cast_to_ljf_handle("obj"),
                         obj.get_handle(*ctx), LJF_ATTR_VISIBLE);
-    ObjectHolder env = create_callee_environment(env0, nullptr);
+
     ASSERT_EQ(obj,
               ctx->get_from_handle(ljf_environment_get(ctx.get(), env, cast_to_ljf_handle("obj"),
                                   LJF_ATTR_VISIBLE)));
 }
 
-TEST(LJFEnvironment, GetNotExistValue) {
-    using namespace ljf::internal;
-    auto ctx = make_temporary_context();
-    ObjectHolder env0 = create_environment(ctx.get());
-    ObjectHolder env = create_callee_environment(env0, nullptr);
+TEST_F(LJFEnvironment, GetNotExistValue) {
 
     ASSERT_THROW(ljf_environment_get(ctx.get(), env,
                                      cast_to_ljf_handle("not_exist"),
